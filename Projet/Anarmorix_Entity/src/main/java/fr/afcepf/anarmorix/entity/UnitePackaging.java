c4440fe210345151f5068a85f016138f2bddc24c
aub91 <aubin.guilhem@gmail.com>
2017-09-17 22:34:10 +0200

    Rajout annotation unitePackaging

--- Projet/Anarmorix_Entity/src/main/java/fr/afcepf/anarmorix/entity/UnitePackaging.java
+++ Projet/Anarmorix_Entity/src/main/java/fr/afcepf/anarmorix/entity/UnitePackaging.java
@@ -4,15 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 /**
  * Entité représentant une unité de packaging.
  */
+@Entity
+@Table(name = "unitePackaging")
 public class UnitePackaging implements Serializable {
 
     /**
