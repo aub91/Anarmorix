4a4f57db25d59dca2b43c58c5fe35cf405eb4bae
aub91 <aubin.guilhem@gmail.com>
2017-09-19 20:53:19 +0200

    zez

--- Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/Api/IDaoJourOuverture.java
+++ Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/Api/IDaoJourOuverture.java
@@ -1,17 +1,9 @@
-package fr.afcepf.anarmorix.data.Api;
-
-import fr.afcepf.anarmorix.entity.JourOuverture;
-import fr.afcepf.anarmorix.entity.PointRelais;
-
+package fr.afcepf.anarmorix.data.api;
 /**
  * 
+ * @author Aubin
+ *
  */
 public interface IDaoJourOuverture {
-
-    /**
-     * @param pointRelais 
-     * @return
-     */
-    public JourOuverture recherche(PointRelais pointRelais);
 
 }
--- Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/api/IDaoAdherent.java
+++ Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/api/IDaoAdherent.java
@@ -1,39 +1,41 @@
-package fr.afcepf.anarmorix.data.api;
+package fr.afcepf.anarmorix.data.Api;
+
+import java.util.List;
 
 import fr.afcepf.anarmorix.entity.Adherent;
 
 /**
- * Fonctionnalités liées à l'entity {@link Adherent}.
- * @author Stagiaire
- *
+ * 
  */
 public interface IDaoAdherent {
 
     /**
-     * Méthode permettant la connexion d'un adhérent.
-     * @param username l'username de l'{@link Adherent} se connectant
-     * @param mdp le mot de passe de l'{@link Adherent} se connectant
-     * @return l'entity {@link Adherent} connecté
+     * @param nom 
+     * @param mdp 
+     * @return
      */
-    Adherent seConnecter(String username, String password);
+    public Adherent seConnecter(String nom, String mdp);
 
     /**
-     * Méthode permettant de faire persister une entity {@link Adherent} en base.
-     * @param adherent l'{@link Adherent} à ajouter.
-     * @return l'{@link Adherent} ajouté à la base.
+     * @param nom 
+     * @return
      */
-    Adherent ajouter(Adherent adherent);
+    public List<Adherent> rechercher(String nom);
 
     /**
-     * Méthode permettant la suppression d'un {@link Adherent} en base.
-     * @param adherent l'{@link Adherent} à supprimer
+     * @return
      */
-    void supprimer(Adherent adherent);
+    public Adherent ajouter();
 
     /**
-     * Méthode permettant de mettre à jour un {@link Adherent} dans la base.
-     * @param adherent l'{@link Adherent} à mettre à jour.
-     * @return l'entity {@link Adherent} mis à jour avec ses nouvelles informations..
+     * @param adherent
      */
-    Adherent mettreAJour(Adherent adherent);
+    public void supprimer(Adherent adherent);
+
+    /**
+     * @param adherent 
+     * @return
+     */
+    public Adherent mettreAJour(Adherent adherent);
+
 }
