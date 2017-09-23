79d7946b33f8f3e2b6a37f4792cd929a38579de8
eldrpr <eloi.dereeper@gmail.com>
2017-09-23 14:48:09 +0200

    Motif Desins

--- Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/api/IDaoMotifDesinscription.java
+++ Projet/Anarmorix_Data_Api/src/main/java/fr/afcepf/anarmorix/data/api/IDaoMotifDesinscription.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import fr.afcepf.anarmorix.entity.Adherent;
 import fr.afcepf.anarmorix.entity.MotifDesinscription;
+import fr.afcepf.anarmorix.exception.AnarmorixException;
 
 /**
- * 
+ * Interface permettant la gestion de l'entité {@link MotifDesinscription}.
  */
 public interface IDaoMotifDesinscription {
 
@@ -14,23 +15,22 @@ public interface IDaoMotifDesinscription {
      * @param nom 
      * @return
      */
-    public List<MotifDesinscription> rechercher(String nom);
+    public List<MotifDesinscription> rechercher() throws AnarmorixException;
 
     /**
      * @param motif 
      * @return
      */
-    public MotifDesinscription ajouter(MotifDesinscription motif);
-
+    public MotifDesinscription ajouter(MotifDesinscription motif) throws AnarmorixException;
     /**
      * @param id
      */
-    public void supprimer(Integer id);
+    public Boolean supprimer(Integer id) throws AnarmorixException;
 
     /**
      * @param adherent 
      * @return
      */
-    public Adherent mettreAJour(Adherent adherent);
+    public MotifDesinscription mettreAJour(Integer id) throws AnarmorixException;
 
 }
--- Projet/Anarmorix_Data_Impl/src/main/java/fr/afcepf/anarmorix/data/impl/DaoMotifDesinscription.java
+++ Projet/Anarmorix_Data_Impl/src/main/java/fr/afcepf/anarmorix/data/impl/DaoMotifDesinscription.java
@@ -2,14 +2,42 @@ package fr.afcepf.anarmorix.data.impl;
 
 import java.util.List;
 
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import fr.afcepf.anarmorix.data.api.IDaoMotifDesinscription;
 import fr.afcepf.anarmorix.entity.Adherent;
+import fr.afcepf.anarmorix.entity.Catalogue;
+import fr.afcepf.anarmorix.entity.Commande;
 import fr.afcepf.anarmorix.entity.MotifDesinscription;
+import fr.afcepf.anarmorix.exception.AnarmorixException;
+import fr.afcepf.anarmorix.exception.AnarmorixExceptionEnum;
 
 /**
- * 
+ * Classe implémentant les méthides de l'{@link IDaoMotifDesinscription}.
  */
+@Remote(IDaoMotifDesinscription.class)
+@Stateless
 public class DaoMotifDesinscription implements IDaoMotifDesinscription {
+
+    /**
+     * Entity manager.
+     */
+    @PersistenceContext(unitName = "Anarmorix_Data_Impl")
+    private EntityManager em;
+
+    /**
+     * Requête permettant de trouver l'ensemble des {@link Catalogue}s d'une exploitation.
+     */
+    private static final String REQ_RECHERCHE =
+            "SELECT m FROM MotifDesinscription";
+
+    /**
+     * Requête permettant de récupérer un catalogue en fonction de son Id.
+     */
+    private static final String REQ_MOTIF_ID = "Select m FROM MotifDesinscription c WHERE m.id = :pId";
 
     /**
      * Default constructor
@@ -18,27 +46,49 @@ public class DaoMotifDesinscription implements IDaoMotifDesinscription {
     }
 
     @Override
-    public List<MotifDesinscription> rechercher(String paramNom) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<MotifDesinscription> rechercher() throws AnarmorixException {
+        try {
+            List<MotifDesinscription> liste =  em.createQuery(REQ_RECHERCHE).getResultList();
+            return liste;
+        } catch (Exception e) {
+             AnarmorixException exc = new AnarmorixException("Message : " + e.getMessage(), AnarmorixExceptionEnum.ERREUR_NON_IDENTIFIEE);
+             throw exc;
+        }
     }
 
     @Override
-    public MotifDesinscription ajouter(MotifDesinscription paramMotif) {
-        // TODO Auto-generated method stub
-        return null;
+    public MotifDesinscription ajouter(MotifDesinscription paramMotif) throws AnarmorixException {
+        try {
+            em.persist(paramMotif);
+            return paramMotif;
+        } catch (Exception e) {
+            AnarmorixException exc =  new AnarmorixException(e.getMessage(), AnarmorixExceptionEnum.ERREUR_NON_IDENTIFIEE);
+            throw exc;
+        }
     }
 
     @Override
-    public void supprimer(Integer paramId) {
-        // TODO Auto-generated method stub
-        
+    public Boolean supprimer(Integer paramId) throws AnarmorixException {
+        try {
+            MotifDesinscription motif = (MotifDesinscription) em.createQuery(REQ_MOTIF_ID).setParameter("pId", paramId).getSingleResult();
+            em.remove(motif);
+            return true;
+        } catch (Exception e) {
+            AnarmorixException exc =  new AnarmorixException(e.getMessage(), AnarmorixExceptionEnum.ERREUR_NON_IDENTIFIEE);
+            throw exc;
+        }
     }
 
     @Override
-    public Adherent mettreAJour(Adherent paramAdherent) {
-        // TODO Auto-generated method stub
-        return null;
+    public MotifDesinscription mettreAJour(Integer paramId) throws AnarmorixException {
+        try {
+            MotifDesinscription motif = (MotifDesinscription) em.createQuery(REQ_MOTIF_ID).setParameter("pId", paramId).getSingleResult();
+            MotifDesinscription updated = em.merge(motif);
+            return updated;
+        } catch (Exception e) {
+            AnarmorixException exc =  new AnarmorixException(e.getMessage(), AnarmorixExceptionEnum.ERREUR_NON_IDENTIFIEE);
+            throw exc;
+        }
     }
 
 }
