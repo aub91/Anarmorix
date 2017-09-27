e5ec65663a901d2eb521c5f3c0fc1cecec8e02dd
eldrpr <eloi.dereeper@gmail.com>
2017-09-27 16:07:38 +0200

    Correction business commande

--- Projet/Anarmorix_Business_Impl/src/main/java/fr/afcepf/anarmorix/business/impl/BusinessCommande.java
+++ Projet/Anarmorix_Business_Impl/src/main/java/fr/afcepf/anarmorix/business/impl/BusinessCommande.java
@@ -4,18 +4,26 @@ import java.util.Date;
 import java.util.List;
 
 import fr.afcepf.anarmorix.business.api.IBusinessCommande;
+import fr.afcepf.anarmorix.data.api.IDaoCommande;
+import fr.afcepf.anarmorix.data.api.IDaoLigneCommande;
+import fr.afcepf.anarmorix.data.impl.DaoCommande;
+import fr.afcepf.anarmorix.data.impl.DaoLigneCommande;
 import fr.afcepf.anarmorix.entity.Commande;
 import fr.afcepf.anarmorix.entity.LigneCommande;
-import fr.afcepf.anarmorix.entity.LignePanier;
+import fr.afcepf.anarmorix.exception.AnarmorixException;
 
 public class BusinessCommande implements IBusinessCommande{
+    
+    private IDaoCommande daoCmd = new DaoCommande();
+    
+    private IDaoLigneCommande daoLg = new DaoLigneCommande();
 
     @Override
-    public Commande creerCommande(List<LignePanier> paramPanier) {
-        Commande cmd = new Commande(null, new Date(), null, "valider", null, null, null);
-        for (LignePanier lignePanier : paramPanier) {
-            LigneCommande lgn = new LigneCommande(null, lignePanier.getQuantite(), null,
-                    null,null, null,null, null, null, null, lignePanier.getProduit(), null);
+    public Commande creerCommande(List<LigneCommande> paramPanier) throws AnarmorixException {
+        Commande cmd = new Commande(null, new Date(), null, "codeValidation", null, null, null);
+        daoCmd.ajouter(cmd);
+        for (LigneCommande lignePanier : paramPanier) {
+            daoLg.ajouter(lignePanier);
         }
         return cmd;
     }
